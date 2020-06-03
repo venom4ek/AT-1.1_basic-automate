31289fd202771f6ecaf9ed5e314d0f581b0596b1
venomek <venom4ek@gmail.com>
2020-06-04 00:21:20 +0300

    added JUnit5

---
+++ src/test/java/ru/netology/service/CashbackHackServiceJUnit5Test.java
@@ -0,0 +1,41 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceJUnit5Test {
+    private CashbackHackService cashback = new CashbackHackService();
+
+    @Test
+    public void shouldBuyInOver1000() {
+        int actual = cashback.remain(1302);
+        int expected = 698;
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldBuyInWhen1000() {
+        int actual = cashback.remain(1000);
+        int expected = 0;
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldBuyInWhen0() {
+        int actual = cashback.remain(0);
+        int expected = 1000;
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldBuyInUnder1000() {
+        int actual = cashback.remain(553);
+        int expected = 447;
+        assertEquals(expected, actual);
+
+    }
+}
