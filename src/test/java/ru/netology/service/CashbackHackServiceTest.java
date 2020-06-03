32e377e4e8b912a2c125f1f4e8e1d97f492de384
venomek <venom4ek@gmail.com>
2020-06-03 23:26:06 +0300

    added test with testNg

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,33 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    private CashbackHackService cashback = new CashbackHackService();
+
+    @Test
+    public void shouldBuyInOver1000() {
+        int actual = cashback.remain(1302);
+        int expected = 698;
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void shouldBuyInWhen0() {
+        int actual = cashback.remain(0);
+        int expected = 1000;
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void shouldBuyInUnder1000() {
+        int actual = cashback.remain(553);
+        int expected = 447;
+        assertEquals(actual, expected);
+
+    }
+}
